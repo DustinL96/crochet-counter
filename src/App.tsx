b5d2ae9dabfd226f3366fb42870a4eb5d7029673
Dustin <dustin@promenadesoftware.com>
2020-12-18 22:49:55 -0800

    Updates

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./App.css";
-import CrochetCounterScreen from "./components/screens/CrochetCounterScreen";
-
-function App() {
-    return <CrochetCounterScreen />;
-}
-
-export default App;
