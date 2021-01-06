7a2b1a4729331f3a5a1f5c3e0392aea481399109
Dustin <dustin@promenadesoftware.com>
2021-01-06 00:54:56 -0800

    Using React Testing Library to test created components.
    Testing and learning new testing library.

---
+++ src/components/screens/__tests__/HomeScreen.test.tsx
@@ -0,0 +1,34 @@
+import "@testing-library/jest-dom";
+import { render, fireEvent, screen } from "@testing-library/react";
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+
+import HomeScreen from "../HomeScreen";
+
+test("Should render Home Screen", () => {
+    render(
+        <BrowserRouter>
+            <HomeScreen />
+        </BrowserRouter>,
+    );
+    // getBy will throw if more than one matches.
+    // Use getAllBy for more than one match.
+    expect(screen.getByText("Stop Counting")).toBeInTheDocument();
+    expect(screen.getByText("Let us do it for you")).toBeInTheDocument();
+    expect(screen.getByText("Get Started")).toBeInTheDocument();
+});
+
+test("Should redirect out of Home Screen after clicking Get Started", () => {
+    // Use Browser Router to allow test to render Redirect component.
+    render(
+        <BrowserRouter>
+            <HomeScreen />
+        </BrowserRouter>,
+    );
+
+    expect(screen.getByText("Stop Counting")).toBeInTheDocument();
+    fireEvent.click(screen.getByText("Get Started"));
+
+    // Query By will be null instead of throwing error if not found.
+    expect(screen.queryByText("Stop Counting")).toBeNull();
+});
